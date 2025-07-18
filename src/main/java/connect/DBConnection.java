97423edd20cfc880964d11f401cf737fa1b49976
Nguyễn Phan Anh <nguyenphananh49@gmail.com>
2025-07-18 14:46:31 +0700

    fix conflict dbconnect

--- src/main/java/connect/DBConnection.java
+++ src/main/java/connect/DBConnection.java
@@ -3,7 +3,6 @@ package connect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,24 +11,21 @@ public class DBConnection {
     private static final String DRIVER_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=FutBall;encrypt=true;trustServerCertificate=true";
     private static final String USER_NAME = "sa";
-    private static final String PASSWORD = "123456";
-
-    private static Connection conn = null;
+    private static final String PASSWORD = "123";
 
     public static Connection getConnection() {
+        Connection conn = null;
         try {
-            if (Objects.isNull(conn) || conn.isClosed()) {
-                Class.forName(DRIVER_NAME);
-                conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-                System.out.println("✅ Connected to database.");
-            }
+            Class.forName(DRIVER_NAME);
+            conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("✅ Connected to database.");
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, "❌ Database connection failed!", ex);
         }
         return conn;
     }
 
-    public static void closeConnection() {
+    public static void closeConnection(Connection conn) {
         try {
             if (conn != null && !conn.isClosed()) {
                 conn.close();
@@ -41,12 +37,14 @@ public class DBConnection {
     }
 
     public static void main(String[] args) {
-        Connection testConn = getConnection();
-        if (testConn != null) {
-            System.out.println("🟢 Connection test successful.");
-        } else {
-            System.out.println("🔴 Connection test failed.");
+        try (Connection testConn = getConnection()) {
+            if (testConn != null) {
+                System.out.println("🟢 Connection test successful.");
+            } else {
+                System.out.println("🔴 Connection test failed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        closeConnection();
     }
 }
