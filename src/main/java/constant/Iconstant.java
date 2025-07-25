54cbb2027d4b2c92e8f90f59ebbd094c19246410
Nguyễn Phan Anh <nguyenphananh49@gmail.com>
2025-07-25 13:36:33 +0700

    fix listener

--- src/main/java/constant/Iconstant.java
+++ src/main/java/constant/Iconstant.java
@@ -3,14 +3,14 @@ package constant;
 public class Iconstant {
 
    
-    public static final String GOOGLE_CLIENT_ID = "xxx";
-    public static final String GOOGLE_CLIENT_SECRET = "xxx";
+    public static final String GOOGLE_CLIENT_ID = "525166296465-4q4lifsqbuqhg7ptinpu0g5bc5tcjkqk.apps.googleusercontent.com";
+    public static final String GOOGLE_CLIENT_SECRET = "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";
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/FootballManagement/login";
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-    public static final String FACEBOOK_CLIENT_ID = "xxx"; 
-    public static final String FACEBOOK_CLIENT_SECRET = "xxx";
+    public static final String FACEBOOK_CLIENT_ID = "1238216694420452";
+    public static final String FACEBOOK_CLIENT_SECRET = "16392d0d52d4f898db904599edc2589e";
     public static final String FACEBOOK_REDIRECT_URI = "http://localhost:8080/FootballManagement/LoginFacebookServlet";
     public static final String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/v19.0/oauth/access_token";
     public static final String FACEBOOK_LINK_GET_USER_INFO = "https://graph.facebook.com/me?fields=id,name,email,picture.type(large)&access_token=";
